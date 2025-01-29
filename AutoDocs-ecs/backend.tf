febe2711a323d1050899d6479557336eba134afa
Or Moshe <orou500@gmail.com>
2025-01-30 00:38:16 +0200

    update to S3 native locking

--- AutoDocs-ecs/backend.tf
+++ AutoDocs-ecs/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "autodocs-terraform-remote-state" // Need to create the bucket manually
-    key            = "autodocs.tfstate" // Exemple - projectName.tfstate
-    region         = "il-central-1" // Exemple - us-east-1
-    profile        = "terraform" // The user profile Exemple - terraform
-    dynamodb_table = "autodocs-terraform-state-lock" # Table must be created manually with partition key "LockID" (String)
-    encrypt        = true                           # Recommended for security
+    bucket       = "autodocs-terraform-remote-state" // Need to create the bucket manually
+    key          = "autodocs.tfstate"                // Exemple - projectName.tfstate
+    region       = "il-central-1"                    // Exemple - us-east-1
+    encrypt      = true                              //Recommended for security
+    use_lockfile = true                              //S3 native locking
   }
 }
--- AutoDocs-ecs/main.tf
+++ AutoDocs-ecs/main.tf
@@ -1,34 +1,33 @@
 # configure aws provider
 
 provider "aws" {
-  region    = var.region
-  profile   = "terraform" // The user profile Exemple - terraform
+  region = var.region
 }
 
 # Create a VPC
 module "vpc" {
-    source                        = "../modules/vpc"
-    region                        = var.region
-    project_name                  = var.project_name
-    vpc_cidr                      = var.vpc_cidr
-    public_subnet_az1_cidr        = var.public_subnet_az1_cidr
+  source                 = "../modules/vpc"
+  region                 = var.region
+  project_name           = var.project_name
+  vpc_cidr               = var.vpc_cidr
+  public_subnet_az1_cidr = var.public_subnet_az1_cidr
 }
 
 # Create a NAT Gateway
 module "nat_gateway" {
-    source                        = "../modules/nat-gateway"
-    public_subnet_az1_id          = module.vpc.public_subnet_az1_id
-    internet_gateway              = module.vpc.internet_gateway
-    vpc_id                        = module.vpc.vpc_id
+  source               = "../modules/nat-gateway"
+  public_subnet_az1_id = module.vpc.public_subnet_az1_id
+  internet_gateway     = module.vpc.internet_gateway
+  vpc_id               = module.vpc.vpc_id
 }
 
 # Create ECS task execution role
 module "ecs-task-execution-role" {
-    source                        = "../modules/ecs-task-execution-role"
-    project_name                  = module.vpc.project_name
+  source       = "../modules/ecs-task-execution-role"
+  project_name = module.vpc.project_name
 }
 
 module "ecr" {
-    source                        = "../modules/ecr"
-    project_name                  = module.vpc.project_name
+  source       = "../modules/ecr"
+  project_name = module.vpc.project_name
 }
